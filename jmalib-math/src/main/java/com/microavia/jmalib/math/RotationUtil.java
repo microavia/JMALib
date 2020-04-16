ccc4fa2f6b0caace3edab1afd9b4b14e91076a96
Anton Babushkin <anton.babushkin@me.com>
2020-04-16 19:33:38 +0200

    RotationUtil: rotateByQuaternion() added

--- jmalib-math/src/main/java/com/microavia/jmalib/math/RotationUtil.java
+++ jmalib-math/src/main/java/com/microavia/jmalib/math/RotationUtil.java
@@ -1,5 +1,7 @@
 package com.microavia.jmalib.math;
 
+import java.util.Arrays;
+
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
 
@@ -27,15 +29,15 @@ public class RotationUtil {
         double cSq = q[2] * q[2];
         double dSq = q[3] * q[3];
         return new double[]{
-            aSq + bSq - cSq - dSq,
-            2.0f * (q[1] * q[2] - q[0] * q[3]),
-            2.0f * (q[0] * q[2] + q[1] * q[3]),
-            2.0f * (q[1] * q[2] + q[0] * q[3]),
-            aSq - bSq + cSq - dSq,
-            2.0f * (q[2] * q[3] - q[0] * q[1]),
-            2.0f * (q[1] * q[3] - q[0] * q[2]),
-            2.0f * (q[0] * q[1] + q[2] * q[3]),
-            aSq - bSq - cSq + dSq
+                aSq + bSq - cSq - dSq,
+                2.0f * (q[1] * q[2] - q[0] * q[3]),
+                2.0f * (q[0] * q[2] + q[1] * q[3]),
+                2.0f * (q[1] * q[2] + q[0] * q[3]),
+                aSq - bSq + cSq - dSq,
+                2.0f * (q[2] * q[3] - q[0] * q[1]),
+                2.0f * (q[1] * q[3] - q[0] * q[2]),
+                2.0f * (q[0] * q[1] + q[2] * q[3]),
+                aSq - bSq - cSq + dSq
         };
     }
 
@@ -44,6 +46,21 @@ public class RotationUtil {
                 Math.atan2(2.0 * (q[0] * q[1] + q[2] * q[3]), 1.0 - 2.0 * (q[1] * q[1] + q[2] * q[2])),
                 Math.asin(2 * (q[0] * q[2] - q[3] * q[1])),
                 Math.atan2(2.0 * (q[0] * q[3] + q[1] * q[2]), 1.0 - 2.0 * (q[2] * q[2] + q[3] * q[3])),
+        };
+    }
+
+    public static double[] rotateByQuaternion(double[] v, double[] q) {
+        double[] qConj = new double[]{q[0], -q[1], -q[2], -q[3]};
+        double[] qV = new double[]{0, v[0], v[1], v[2]};
+        return Arrays.copyOfRange(quaternionMult(quaternionMult(q, qV), qConj), 1, 4);
+    }
+
+    private static double[] quaternionMult(double[] q1, double[] q2) {
+        return new double[]{
+                q1[0] * q2[0] - q1[1] * q2[1] - q1[2] * q2[2] - q1[3] * q2[3],
+                q1[0] * q2[1] + q1[1] * q2[0] + q1[2] * q2[3] - q1[3] * q2[2],
+                q1[0] * q2[2] + q1[2] * q2[0] + q1[3] * q2[1] - q1[1] * q2[3],
+                q1[0] * q2[3] + q1[3] * q2[0] + q1[1] * q2[2] - q1[2] * q2[1],
         };
     }
 }
